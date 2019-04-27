2a734903e26164077aa58ef5ad4f43d3c9ce0b11
Aspect26 <flimmelj@gmail.com>
2019-04-27 17:56:41 +0300

    Added fountain prefab

---
+++ Assets/Scripts/Objects/Fountain.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.Objects
+{
+    public class Fountain : MonoBehaviour
+    {
+
+        private UIBehaviour ui;
+
+        void Start()
+        {
+            this.ui = GameObject.FindGameObjectWithTag("UI").GetComponent<UIBehaviour>();
+        }
+
+        void OnTriggerEnter2D(Collider2D col)
+        {
+            this.ui.Interact(this);
+        }
+
+        void OnTriggerExit2D(Collider2D col)
+        {
+            this.ui.StopInteract(this);
+        }
+
+    }
+}
