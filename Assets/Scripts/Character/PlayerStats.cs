9085e4e68f548f5b1a4789689d8e9d44ce2879de
Michal Fibich <Michal.Fibichidis@gmail.com>
2019-04-27 21:37:52 +0200

    - Shooting speed added to player stats

--- Assets/Scripts/Character/PlayerStats.cs
+++ Assets/Scripts/Character/PlayerStats.cs
@@ -5,15 +5,18 @@ namespace Assets.Scripts.Character
     public class PlayerStats : MonoBehaviour
     {
         private const int STARTING_HEALTH = 255;
+        private const float STARTING_PPS = 0.45f;
 
         public int CurrentHitPoints { get; private set; }
         public int MaxHitPoints { get; private set; }
+        public float ProjectilesPerSecond { get; private set; }
 
         void Start()
         {
             this.MaxHitPoints = STARTING_HEALTH;
             this.CurrentHitPoints = STARTING_HEALTH;
-            GameObject.FindGameObjectWithTag("UI").GetComponent<Assets.Scripts.UI.UIBehavior>().SetHealth(STARTING_HEALTH, STARTING_HEALTH);
+            this.ProjectilesPerSecond = STARTING_PPS;
+            GameObject.FindGameObjectWithTag("UI").GetComponent<UI.UIBehavior>().SetHealth(STARTING_HEALTH, STARTING_HEALTH);
         }
 
         public int TryTakeDamagePercentage(float percentage)
