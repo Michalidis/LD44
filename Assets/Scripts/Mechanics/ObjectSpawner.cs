d66b50e694b672357ca9d44a40fad7c67d2104c2
Aspect26 <flimmelj@gmail.com>
2019-04-28 21:15:05 +0300

    Added wave counter + increasing number of enemies over waves

--- Assets/Scripts/Mechanics/ObjectSpawner.cs
+++ Assets/Scripts/Mechanics/ObjectSpawner.cs
@@ -5,13 +5,16 @@ namespace Assets.Scripts.Mechanics
     public class ObjectSpawner : MonoBehaviour
     {
         private static Vector2 NO_HIT_VECTOR = new Vector2(55555f, 55555f);
+        private static int INCREASE_COUNT_EVERY_X_ROUND = 5; 
 
         [SerializeField] private GameObject[] objectsToSpawn;
         [SerializeField] private int count;
         [SerializeField] private float interval;
         [SerializeField] private float delay;
         [SerializeField] private float radius;
+
         private System.Random random = new System.Random();
+        private int currentWave = 0;
     
         void Start()
         {
@@ -20,6 +23,13 @@ namespace Assets.Scripts.Mechanics
 
         private void Spawn()
         {
+            this.currentWave++;
+
+            if (this.currentWave % INCREASE_COUNT_EVERY_X_ROUND == 0)
+            {
+                this.count++;
+            }
+
             for (var i = 0; i < this.count; ++i)
             {
                 var spawnPosition2D = this.getSpawnPosition();
@@ -30,6 +40,8 @@ namespace Assets.Scripts.Mechanics
                 var index = this.random.Next(this.objectsToSpawn.Length);
                 Instantiate(objectsToSpawn[index], spawnPosition2D, Quaternion.identity);
             }
+
+            GameObject.FindGameObjectWithTag("UI").GetComponent<UI.UIBehavior>().SetWave(this.currentWave);
         }
 
         private Vector2 getSpawnPosition()
@@ -37,11 +49,12 @@ namespace Assets.Scripts.Mechanics
             var myPosition = this.gameObject.transform.position;
             var myPosition2D = new Vector2(myPosition.x, myPosition.y);
             var player = GameObject.FindGameObjectWithTag("Player");
+            var spawnPosition2D = new Vector2();
 
             for (int i = 0; i < 100; ++i)
             {
                 var spawnPositionOffset2D = Random.insideUnitCircle * this.radius;
-                var spawnPosition2D = myPosition2D + spawnPositionOffset2D;
+                spawnPosition2D = myPosition2D + spawnPositionOffset2D;
                 var directionToMe = myPosition2D - spawnPosition2D;
 
                 var raycastHit = Physics2D.Raycast(spawnPosition2D, directionToMe, this.radius * 1.5f);
@@ -56,7 +69,7 @@ namespace Assets.Scripts.Mechanics
                 }
             }
 
-            Debug.Log("NOT SPAWNED!");
+            Debug.Log("Spawned at possible wrong position!");
             return NO_HIT_VECTOR;
         }
     }
