62ce9c3edd7260cfcf27e7eddb8d7f781070213f
Michal Fibich <Michal.Fibichidis@gmail.com>
2019-04-28 11:25:41 +0200

    - Added basic item management scripts

---
+++ Assets/Scripts/Items/ItemPickupManager.cs
@@ -0,0 +1,50 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickupManager : MonoBehaviour
+{
+    bool isPickedUp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPickedUp = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isPickedUp)
+        {
+            return;
+        }
+
+        foreach (var contact in collision.contacts)
+        {
+            if (contact.collider.tag == "Player")
+            {
+                ItemAttributes itemAttributes = GetComponent<ItemAttributes>();
+                if (itemAttributes)
+                {
+                    contact.collider.GetComponent<ItemManager>().PickUpItem(itemAttributes.Item);
+                    StartCoroutine(PlanDestroy());
+                }
+            }
+        }
+    }
+
+    IEnumerator PlanDestroy()
+    {
+        isPickedUp = true;
+        GetComponent<PolygonCollider2D>().enabled = false;
+        GetComponent<SpriteRenderer>().DOFade(0.0f, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
+    }
+}
