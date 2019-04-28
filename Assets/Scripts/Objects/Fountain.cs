d9ae6dceddabc7df71a43d43954aa973bd2ea171
Michal Fibich <Michal.Fibichidis@gmail.com>
2019-04-28 22:37:05 +0200

    - loot from fountain now appears on the opposite side as the player is standing

--- Assets/Scripts/Objects/Fountain.cs
+++ Assets/Scripts/Objects/Fountain.cs
@@ -22,6 +22,13 @@ namespace Assets.Scripts.Objects
             {
                 GameObject.FindGameObjectWithTag("UI").GetComponent<Assets.Scripts.UI.UIBehavior>().SetHealth(playerStats.CurrentHitPoints, playerStats.MaxHitPoints);
                 GameObject.Instantiate(bloodFountain, this.transform.position, this.transform.rotation);
+                GameObject loot = GetComponent<LootManager>().GetLootItem();
+                if (loot)
+                {
+                    Vector3 item_spawn_position = player.transform.position - transform.position;
+                    item_spawn_position.z = 0;
+                    GameObject instance = Instantiate(loot, transform.position - 0.43f * item_spawn_position.normalized, transform.rotation);
+                }
                 GameObject.Destroy(this.gameObject);
             }
         }
