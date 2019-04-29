5686568d96229e5e90591d4a793d038b61b458d2
Michal Fibich <Michal.Fibichidis@gmail.com>
2019-04-29 17:29:00 +0200

    - Character prefab update - Ruby of Fire sound

--- Assets/Scripts/Character/SpecialsHolder.cs
+++ Assets/Scripts/Character/SpecialsHolder.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 public class SpecialsHolder : MonoBehaviour
 {
     public GameObject Fireball;
+    public AudioClip Fireball_sound;
+    public AudioSource audio_player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audio_player = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void PlayFireballSound()
+    {
+        audio_player.clip = Fireball_sound;
+        audio_player.Play();
     }
 }
