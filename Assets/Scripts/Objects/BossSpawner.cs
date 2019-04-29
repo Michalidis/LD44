35e4108007421ba61cc18e0807419945be4ded6d
Aspect26 <flimmelj@gmail.com>
2019-04-29 22:20:03 +0300

    Added boss spawner

---
+++ Assets/Scripts/Objects/BossSpawner.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Interfaces;
+using UnityEngine;
+
+namespace Assets.Scripts.Objects
+{
+    public class BossSpawner : Interactable
+    {
+
+        [SerializeField] private GameObject bossPrefab;
+
+        public override void PlayerInteract(GameObject player)
+        {
+            GameObject.Instantiate(bossPrefab, this.gameObject.transform);
+            GameObject.Destroy(this.gameObject);
+            Debug.Log("CREATED BOSS");
+        }
+
+        public override string GetInteractDescriptionText()
+        {
+            return "This looks really dangerous!";
+        }
+    }
+}
