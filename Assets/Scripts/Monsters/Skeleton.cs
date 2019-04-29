7c40202e97da8f8c3bee1ba6e0f916bd5294b0e9
Michal Fibich <Michal.Fibichidis@gmail.com>
2019-04-29 18:23:49 +0200

    - Skeleton projectile sound - skeleton movement sounds - credits update

--- Assets/Scripts/Monsters/Skeleton.cs
+++ Assets/Scripts/Monsters/Skeleton.cs
@@ -4,18 +4,45 @@ using UnityEngine;
 public class Skeleton : MonsterMovement
 {
     private Vector2 velocity = new Vector2(0.2f, 0.2f);
+    public AudioClip[] SkeletonMovementClips;
+    private AudioSource soundPlayer;
+    private float soundEstimate;
 
     private void Start()
     {
         Speed = 0.5f;
         this.Init();
         this.Spawn();
+        this.soundPlayer = this.GetComponent<AudioSource>();
+        if (SkeletonMovementClips.Length != 0)
+        {
+            this.soundPlayer.clip = this.SkeletonMovementClips[this.random.Next(this.SkeletonMovementClips.Length)];
+        }
     }
 
     private void Update()
     {
+        PlayMovementSound();
         this.body.velocity = this.velocity * this.Speed;
         this.SetAnimator(this.body.velocity);
+    }
+
+    private void PlayMovementSound()
+    {
+        if (soundEstimate < 0)
+        {
+            this.soundEstimate = this.soundPlayer.clip.length + 5 + this.random.Next(5);
+            if (SkeletonMovementClips.Length != 0)
+            {
+                this.soundPlayer.clip = this.SkeletonMovementClips[this.random.Next(this.SkeletonMovementClips.Length)];
+
+                this.soundPlayer.Play();
+            }
+        }
+        else
+        {
+            soundEstimate -= Time.deltaTime;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -28,7 +55,7 @@ public class Skeleton : MonsterMovement
 
         Vector2 dist = new Vector2(thisPos.x - collisionPoint.x, thisPos.y - collisionPoint.y).normalized;
         var randVec = new Vector2(dist.x * 0.9f * this.random.Next(100) / 100.0f, dist.y * 0.9f * this.random.Next(100) / 100.0f);
-        
+
         Vector3 plPosition = this.Player.gameObject.transform.position;
         float plX = plPosition.x;
         float plY = plPosition.y;
